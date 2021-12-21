ee3ee64aa4ffb00ca3a27446a3843ef3863543ff
Semyon Nikolaev <nsa92@ya.ru>
2021-12-21 21:42:15 +0300

    fix typo

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -87,7 +87,7 @@ func TestStatHandler(t *testing.T) {
 		t.Run(tt.testName, func(t *testing.T) {
 			request := httptest.NewRequest(http.MethodPost, tt.urlPath, nil)
 			w := httptest.NewRecorder()
-			guageChan := make(chan server.GuageDataUpdate, 1024)
+			guageChan := make(chan server.GaugeDataUpdate, 1024)
 			counterChan := make(chan server.CounterDataUpdate, 1024)
 			// h := http.HandlerFunc(UserViewHandler(tt.users))
 			h := server.MakeHandler(guageChan, counterChan, "/")
