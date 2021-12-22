9e17a89ef1575d5921a5a7a0cad2939a2ccca87a
Semyon Nikolaev <nsa92@ya.ru>
2021-12-22 18:53:20 +0300

    remove all changes

--- cmd/agent/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/nikolaevs92/Practicum/internal/agent"
-)
-
-func main() {
-	agent.RunAgentDefault()
-}
--- cmd/server/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/nikolaevs92/Practicum/internal/server"
-)
-
-func main() {
-	server.RunServerDefault()
-}
