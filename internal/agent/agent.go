647e09edbfc2fd42144dc1634b4505604b058f0a
Semyon Nikolaev <nsa92@ya.ru>
2022-01-19 17:00:51 +0300

    add TotalAlloc and Frees to collector

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -116,6 +116,8 @@ func (collector *CollectorAgent) PostOneCounterStat(metricName string, metricVal
 
 func (collector *CollectorAgent) Report(t time.Time) {
 	go collector.PostOneGaugeStat("Alloc", float64(collector.stats.Alloc))
+	go collector.PostOneGaugeStat("TotalAlloc", float64(collector.stats.TotalAlloc))
+	go collector.PostOneGaugeStat("Frees", float64(collector.stats.Frees))
 	go collector.PostOneGaugeStat("BuckHashSys", float64(collector.stats.BuckHashSys))
 	go collector.PostOneGaugeStat("Frees", float64(collector.stats.Frees))
 	go collector.PostOneGaugeStat("GCCPUFraction", float64(collector.stats.GCCPUFraction))
