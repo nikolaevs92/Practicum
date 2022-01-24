572a33ad3b600e5df260c2ebaa6c7f74a8a1301b
Semyon Nikolaev <nsa92@ya.ru>
2022-01-24 16:35:20 +0300

    finally?

--- internal/datastorage/metrics.go
+++ internal/datastorage/metrics.go
@@ -30,30 +30,26 @@ func (m *Metrics) MarshalJSON() ([]byte, error) {
 	switch m.MType {
 	case CounterTypeName:
 		aliasValue := &struct {
-			ID    string  `json:"id"`              // имя метрики
-			MType string  `json:"type"`            // параметр, принимающий значение gauge или counter
-			Value float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
-			Delta uint64  `json:"delta"`
+			ID    string `json:"id"`   // имя метрики
+			MType string `json:"type"` // параметр, принимающий значение gauge или counter
+			Delta uint64 `json:"delta"`
 		}{
 			ID:    m.ID,
 			MType: m.MType,
 			Delta: m.Delta,
-			Value: 0,
 		}
 		body, _ := json.Marshal(aliasValue)
 		log.Println(string(body))
 		return body, nil
 	case GaugeTypeName:
 		aliasValue := &struct {
-			ID    string  `json:"id"`   // имя метрики
-			MType string  `json:"type"` // параметр, принимающий значение gauge или counter
-			Delta uint64  `json:"delta,omitempty"`
+			ID    string  `json:"id"`    // имя метрики
+			MType string  `json:"type"`  // параметр, принимающий значение gauge или counter
 			Value float64 `json:"value"` // значение метрики в случае передачи gauge
 		}{
 			ID:    m.ID,
 			MType: m.MType,
 			Value: m.Value,
-			Delta: 0,
 		}
 		body, _ := json.Marshal(aliasValue)
 		log.Println(string(body))
