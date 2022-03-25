aec8958c37605bb30ff1091052af7f44173b5a3b
Semyon Nikolaev <nsa92@ya.ru>
2022-03-25 16:51:32 +0300

    add logs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	log.Println("FINDME")
 	adress := pflag.StringP("adress", "a", config.DefaultServer, "")
 	storeInterval := pflag.DurationP("strore-interval", "i", config.DefaultStoreInterval, "")
 	storeFile := pflag.StringP("store-file", "f", config.DefaultStoreFile, "")
